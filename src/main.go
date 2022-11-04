38351d4b686ca6d62da6a1a1d2c92f0fba0fa839
bienpn <biennhutptit@gmail.com>
2022-11-04 07:58:01 +0700

    db connection

--- src/main.go
+++ src/main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	usertrpt "cmc/module/users/transport"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"github.com/gin-gonic/gin"
-	usertrpt "cmc/module/users/transport"
 )
 
-func main()  {
-	dsn := "root:root@tcp(127.0.0.1:3306)/cmc?charset=utf8mb4&parseTime=True&loc=Local"
+func main() {
+	fmt.Println("test")
+	dsn := "root:password@tcp(mysqlcontainer)/cmc?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-        fmt.Println("err")
-    }
+		fmt.Println("err")
+	}
 	fmt.Println(&db)
 
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
 	{
-		v1.POST("/user", usertrpt.HanleCreateItem(db))  // updated
+		v1.POST("/user", usertrpt.HanleCreateUser(db)) // updated
 		//...
 	}
+	router.Run(":9000")
 }
