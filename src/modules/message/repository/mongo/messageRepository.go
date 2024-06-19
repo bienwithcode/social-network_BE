06925d800e6f09d1d68777f853e5469865847154
bien-smd <bienngocpham@gmail.com>
2024-06-19 14:05:48 +0700

    [message] get messages

--- src/modules/message/repository/mongo/messageRepository.go
+++ src/modules/message/repository/mongo/messageRepository.go
@@ -113,11 +113,64 @@ func (storage *mongodbStorage) GetConversations(ctx context.Context, authUserId 
 }
 
 func (storage *mongodbStorage) GetMessages(ctx context.Context, authUserId, userId string) ([]*domain.Message, error) {
-	// filter := bson.M{
-	// 	"$or": []bson.M{
-	// 		{"sender": authUserId, "receiver": userId},
-	// 		{"sender": userId, "receiver": authUserId},
-	// 	},
-	// }
-	return nil, nil
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{
+				"$and": []bson.M{
+					{
+						"$or": []bson.M{
+							{"sender": authUserId},
+							{"receiver": authUserId},
+						},
+					},
+					{
+						"$or": []bson.M{
+							{"sender": userId},
+							{"receiver": userId},
+						},
+					},
+				},
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from":         "users",
+				"localField":   "sender",
+				"foreignField": "_id",
+				"as":           "sender",
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from":         "users",
+				"localField":   "receiver",
+				"foreignField": "_id",
+				"as":           "receiver",
+			},
+		},
+		{
+			"$project": bson.M{
+				"sender.password":   0,
+				"receiver.password": 0,
+			},
+		},
+		{
+			"$sort": bson.M{
+				"updatedAt": 1,
+			},
+		},
+	}
+
+	collection := storage.db.Collection(domain.Message{}.TableName())
+	cursor, err := collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []*domain.Message
+
+	if err := cursor.All(ctx, &messages); err != nil {
+		return nil, err
+	}
+	return messages, nil
 }
